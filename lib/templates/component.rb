1d8bfb12d8b83b054875b17639fa867249fb5bab
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-10-03 16:36:23 -0500

    Added reference to each component file

--- lib/templates/component.rb
+++ lib/templates/component.rb
@@ -1,2 +1,6 @@
 class <%= file_name.camelize %>Component < ApplicationComponent
+
+  # NOTICE: Do not remove this line this references the template path for the actual component
+  self.template_path = File.dirname("#{Rails.root}/app/templates/#{self.class.name.underscore}")
+
 end
