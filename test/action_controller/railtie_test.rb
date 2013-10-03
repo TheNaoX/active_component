6fbb41f5e2034dc4f08b4c390f2fe924a81357bd
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-10-03 13:13:33 -0500

    Renamed path

--- test/action_controller/railtie_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ActiveComponent::RailtieTest < ActiveSupport::TestCase
-
-  test "get the controller context" do
-    assert FooController.new.render_component
-  end
-
-end
--- test/action_controller/renderable_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ActiveComponent::RenderableTest < ActiveSupport::TestCase
-
-  test "renders a component" do
-    assert_kind_of(String, FooController.new.render_component)
-  end
-
-end
