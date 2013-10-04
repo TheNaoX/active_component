001ddb14f963f366fcef8a1807b44b5d5e202301
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-10-04 17:39:53 -0500

    Updated documentation and test

--- lib/active_component/renderable.rb
+++ lib/active_component/renderable.rb
@@ -1,13 +1,10 @@
 require 'active_support/concern'
 
 # 
-# Work In Progress
+# This provides a clean implementation for rendering your template
+# inside your view.
 #
-# The idea is to provide a clean rails-like methods to
-# render view components nicely from the view or even respond
-# with them from a controller instance
-#
-# render_component(:view_component, var: var)
+# <%= render_component(:view, var: var) %>
 #
 module ActiveComponent
   module Renderable
--- test/active_component/renderable_test.rb
+++ test/active_component/renderable_test.rb
@@ -5,13 +5,13 @@ class ActiveComponent::RenderableTest < ActiveSupport::TestCase
   test "renders a component" do
     FooComponent.any_instance.stubs(:render).returns("")
     assert_nothing_raised TypeError do
-      ActionView::Base.new.render_component(:foo, {})
+      ActionView::Base.new.render_component(:foo)
     end
   end
 
   test "gets a component instance from symbol" do
     FooComponent.any_instance.stubs(:render).returns("")
-    assert_kind_of String, ActionView::Base.new.render_component(:foo, {})
+    assert_kind_of String, ActionView::Base.new.render_component(:foo)
   end
 
 end
