fb99c5763b44f100210a9306f93994dbbbfc4c97
Jesus Romero <jose.romero@tangosource.com>
2013-10-07 13:35:37 -0500

    deleted binding.pry

--- lib/active_component/railtie.rb
+++ lib/active_component/railtie.rb
@@ -4,7 +4,6 @@
 require 'rails'
 module ActiveComponent
   class Railtie < Rails::Railtie
-    binding.pry
     initializer "Appends a method to the action controller class" do
       #ActiveSupport.on_load(:action_controller) do
         ActionController::Base.send(:include, ActiveComponent::Context)
