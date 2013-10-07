183db76bd1bbf5b5be9d8b14e7009424f286c0d4
Jesus Romero <jose.romero@tangosource.com>
2013-10-07 13:58:48 -0500

    move sentence to include ActiveComponent::Contexte

--- lib/active_component/railtie.rb
+++ lib/active_component/railtie.rb
@@ -4,6 +4,7 @@
 require 'rails'
 module ActiveComponent
   class Railtie < Rails::Railtie
+    ActionController::Base.send(:include, ActiveComponent::Context)
     initializer "Appends a method to the action controller class" do
       #ActiveSupport.on_load(:action_controller) do
         ActionController::Base.send(:include, ActiveComponent::Context)
