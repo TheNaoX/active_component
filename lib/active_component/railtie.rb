1d759f67bc9acd28ad4d116b119bcea86536bc58
Jesus Romero <jose.romero@tangosource.com>
2013-10-07 14:49:38 -0500

    deleted unnecesary

--- lib/active_component/railtie.rb
+++ lib/active_component/railtie.rb
@@ -1,14 +1,10 @@
 #
 # Autoinclude some of the modules from the controller top level
 #
-require 'rails'
 module ActiveComponent
   class Railtie < Rails::Railtie
     ActionController::Base.send(:include, ActiveComponent::Context)
     initializer "Appends a method to the action controller class" do
-      #ActiveSupport.on_load(:action_controller) do
-        ActionController::Base.send(:include, ActiveComponent::Context)
-      #end
     end
   end
 end
